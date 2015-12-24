c04cd20e7af2b4bf6651878891c05be14250caea
robert fenner <robert fenner>
2015-12-23 23:34:16 -0500

    working on items

---
+++ Items/Interfaces/IItemBase.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DNDSim.Items.Interfaces
+{
+    public interface IItemBase
+    {
+        void Appraise(int appraiseSkill);
+
+        string Name { get; set; }
+
+        int Charges { get; set; }
+    }
+}
---
+++ Items/ItemFactory.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DNDSim.Items.Armor.Interfaces;
+using DNDSim.Items.Interfaces;
+using DNDSim.Items.Potion.Interfaces;
+using DNDSim.Items.Weapons.Interfaces;
+
+namespace DNDSim.Items
+{
+    public static class ItemFactory
+    {
+        public static void CreateTreasure(int encounterLevel)
+        {
+            
+        }
+
+        public static IArmorBase CreateArmor()
+        {
+            throw new NotImplementedException();
+        }
+
+        public static IPorionBase CreatePotion()
+        {
+            throw new NotImplementedException();
+        }
+
+        public static IWeaponBase CreateWeapon()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
